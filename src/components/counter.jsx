da1a79079da44816a66c3cf435ab8b840df816d6
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-18 12:35:24 +0530

    Adding classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,17 @@ class Counter extends Component {
     };
 
     styles = {
-        fontSize : 50,
+        fontSize : 10,
         fontWeight : 'bold'
     };
 
     render() { 
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+
         return ( 
             <React.Fragment>
-                <span style = { this.styles } className="badge badge-primary m-2" >{this.formatCount()}</span>
+                <span style = { this.styles } className = { classes } > { this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
          );
