4739df4c51200330e88d5f9bc26e90efedfb9f58
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-19 13:59:52 +0530

    Conditional Rendering in React and truthy && falsy - no ngIf in React like Angular

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,11 +16,16 @@ class Counter extends Component {
             <React.Fragment>
                 <span style = { this.styles } className = { this.getBadgeClasses() }> { this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                { this.state.tags.length === 0 && "Please add some tags" }
+                { this.conditionalRender() }              
             </React.Fragment>
          );
+    }
+
+    conditionalRender(){
+        if(this.state.tags.length === 0) return <p>There are no tags</p>
+
+        return <ul>{this.state.tags.map( tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
     getBadgeClasses() {
