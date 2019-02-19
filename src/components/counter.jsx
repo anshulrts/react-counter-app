2b18fd424acffec7f97d80358aac3461b1af7c19
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-19 14:12:21 +0530

    Avoid - Removing unnecessary code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,17 +15,9 @@ class Counter extends Component {
         return ( 
             <React.Fragment>
                 <span style = { this.styles } className = { this.getBadgeClasses() }> { this.formatCount() }</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                { this.state.tags.length === 0 && "Please add some tags" }
-                { this.conditionalRender() }              
+                <button className="btn btn-secondary btn-sm">Increment</button>           
             </React.Fragment>
          );
-    }
-
-    conditionalRender(){
-        if(this.state.tags.length === 0) return <p>There are no tags</p>
-
-        return <ul>{this.state.tags.map( tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
     getBadgeClasses() {
