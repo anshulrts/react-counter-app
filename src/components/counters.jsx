0451995d72826c8083665822d51cef2681256c6a
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-20 12:51:57 +0530

    Added Reset button and implemented its event. But it will not work right now, as every Counter component has its own local state which is holding the value of count

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
         ]
      };
 
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+    }
+
     // In order to implement delete button functionality, we have to delete the counter from the state of Counters component
     // Therefore, on the click of delete button, we will raise an onDelete Event from the Counter component and pass the id to be
     // deleted
@@ -23,6 +31,7 @@ class Counters extends Component {
     render() { 
         return (
             <div>
+                <button onClick = {this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button><br />
                 { this.state.counters.map(
                     // All of the attributes of <Counter ... /> will be available in the Child component using this.props
                     // But we can also associate child property to props in between <Counter>...</Counter>
