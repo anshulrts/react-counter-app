254bb4d25f8419cbe6aebbce50aebcdf9f4e872e
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-20 12:23:21 +0530

    Implementing delete functionality - Child to Parent Communication

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,7 +15,7 @@ class Counter extends Component {
         // Changing variable name count -> value in order to maintain consistency.
         // Basically, the attributes defined in the parent component can be accessed in the child component using
         // this.props object
-        value : this.props.value,
+        value : this.props.counter.value,
         tags : ["tag1", "tag2", "tag3"]
     };
 
@@ -35,14 +35,15 @@ class Counter extends Component {
     };
 
     render() {
-        console.log(this.props);
         return (
             <React.Fragment>
                 {/* Accessing props in Child Component */}
-                {/* <h2>Counter # {this.props.id} </h2> */}
+                {/* <h2>Counter # {this.props.counter.id} </h2> */}
                 <span style = { this.styles } className = { this.getBadgeClasses() }> { this.formatCount() }</span>
                 <button onClick = { () => this.handleIncrement({id : 1}) } className="btn btn-secondary btn-sm">Increment</button>
-                <button className="btn btn-danger btn-sm m-2">Delete</button>
+                {/* Passing the id to be deleted to parent component, as this entry needs to be deleted from 
+                Counters.state.list */}
+                <button onClick = { () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">Delete</button>
                 <br />           
             </React.Fragment>
          );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,17 +10,24 @@ class Counters extends Component {
             {id : 4, value : 0}
         ]
      };
+
+    // In order to implement delete button functionality, we have to delete the counter from the state of Counters component
+    // Therefore, on the click of delete button, we will raise an onDelete Event from the Counter component and pass the id to be
+    // deleted
+    handleDelete = counterId => {
+        console.log('Event Delete called.', counterId);
+        const counters = this.state.counters.filter(row => row.id != counterId);
+        this.setState({ counters });
+    }
+    
     render() { 
         return (
             <div>
                 { this.state.counters.map(
                     // All of the attributes of <Counter ... /> will be available in the Child component using this.props
                     // But we can also associate child property to props in between <Counter>...</Counter>
-                    // Here, we are setting it to
                     counter =>
-                    <Counter key={ counter.id } value = { counter.value } id = { counter.id }>
-                        <h2>Title</h2>
-                    </Counter>
+                    <Counter key={ counter.id } onDelete = { this.handleDelete } counter = { counter } />
                     )
                 }
             </div>
