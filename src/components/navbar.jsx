074ad05f34db5b4f0e29f76ed6b45789a60a5260
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-21 15:44:38 +0530

    Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Total Count <span className="badge badge-pill badge-secondary">{ this.props.totalCounters }</span>
-                </a>
-            </nav>
-         );
-    }
-}
+
+// Stateless Functional Component
+// Since the Navbar doesn't have any state and is just displaying a prop, we can make it a SFC.
+// In SFC, we don't necessarily need a class to import from Component. A function is enough. And they receive props in parameters
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Total Count <span className="badge badge-pill badge-secondary">{ props.totalCounters }</span>
+            </a>
+        </nav>
+     );
+};
  
 export default NavBar;
